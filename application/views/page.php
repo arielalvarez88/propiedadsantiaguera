41a06d75fbbf4b9cc969009e158d77143a5bf64b
Ariel <ariel@ariel-desktop.(none)>
2011-10-14 16:12:41 -0400

    removing login-password from clear password

--- application/views/page.php
+++ application/views/page.php
@@ -174,7 +174,7 @@ $thisPage = str_replace('/', '-', uri_string());
                     <img src="/images/login/loginTitle.png" alt="Login" id="login-title"/>
                     <input id="login-email" type="text" name="login-email" class="login-input" /><br/>
                     
-                    <input id="login-password-clear" type="text" name="login-password" class="login-input" /><br/>
+                    <input id="login-password-clear" type="text" class="login-input" /><br/>
                     <input id="login-password" type="password" name="login-password" class="login-input" style="display:none;"/>
                     <input id="login-submit" type="image" src="<?php echo base_url();?>images/login/loginButton.png" alt="login"/> 
                     <a href="#" id="login-password-reset-button">&iquest;Olvido su contrase&ntilde;a?</a>
