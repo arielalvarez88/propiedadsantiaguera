b039be529d7efac4af4a68fa4fc39340ae863c46
xereu <xereu@xereu-laptop.(none)>
2011-10-21 17:34:28 -0400

    Area de planes 50%

---
+++ application/views/blocks/planes_view.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<table id="planes">
+    	<colgroup class="info_col" span="1"/></colgroup>
+    	<colgroup class="basic_plan_col" /></colgroup>
+    	<colgroup class="plus_plan_col" /></colgroup>
+    	<colgroup class="agent_plan_col" /></colgroup>
+    	<colgroup class="realty_plan_col" /></colgroup>
+    
+    
+        <tr>
+            <th scope="col"></th><th scope="col" class="regular_title">BASICO</th><th scope="col" class="plus_title">PLUS</th><th scope="col" class="regular_title">AGENTE</th><th scope="col" class="regular_title">INMOBILIARIA</th>
+        </tr>
+    
+ 
+        <tr>
+            <td>NUMERO DE PROPIEDADES</td> <td>1</td> <td>5</td> <td>10</td> <td>25</td>
+        </tr>
+        <tr>
+            <td>DURACION POR PROPIEDAD</td> <td>30</td> <td>30</td> <td>30</td> <td>30</td>
+        </tr>
+        <tr>
+            <td>FOTOS</td> <td>10</td> <td>10</td> <td>15</td> <td>15</td>
+        </tr>
+        <tr>
+            <td>VIDEO</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td>DESCUENTO EN BANNERS PUBLICITARIOS</td> <td>5%</td> <td>5%</td> <td>10%</td> <td>10%</td>
+        </tr>
+        <tr>
+            <td>CREACION DE SOLICITUD Y ENVIAR PROPUESTA DE SOLICITUDES</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td>INFORMACION EN INGLES Y ESPA&Ntilde;OL</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td>LOCALIZACION DE LA PROPIEDAD EN GOOGLE MAPS</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td>IMPRESION DE LA PROPIEDAD EN FORMATO PDF</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td>VIDEOS COMPARTIDOS EN EL WEBSITE DEL USUARIO</td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+        <tr>
+            <td></td> <td>RD$1,500</td> <td>RD$6,000</td> <td>RD$10,000</td> <td>RD$20,000</td> 
+        </tr>
+        <tr>
+            <td></td> <td>RD$1,500/Propiedad</td> <td>RD$1,200/Propiedad</td> <td>RD$1,000/Propiedad</td> <td>RD$800/Propiedad</td>
+        </tr>
+        <tr>
+            <td></td> <td>Elegir Plan</td> <td>Elegir Plan</td> <td>Elegir Plan</td> <td>Elegir Plan</td>
+        </tr>
+        <tr>
+            <td></td> <td></td> <td></td> <td></td> <td></td>
+        </tr>
+
+</table>
