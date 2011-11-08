2a560bec52e9698ebfbf9c70bd76bb04f55a0a47
Ariel <ariel@ariel-desktop.(none)>
2011-11-08 18:27:54 -0400

    uploading banner pictures

---
+++ application/views/blocks/panels_account.php
@@ -0,0 +1,71 @@
+<div id="panels-account-wrapper">
+    
+    <div id="panels-account">
+        <p>Actualmente usted dispone de <?php echo $user->posts_left;?> espacios disponibles para publicar sus propiedades</p>
+        <h2>Precio y Planes</h2>
+        <p>Adquiere m&aacute;s espacios y disfruta de todos nuestros beneficios.</p>
+        
+            <table id="panels-account-plans">
+                <tr id="panels-account-plans-header-row">
+                    <th>Plan</th>
+                     <th>Propiedades</th>
+                     <th>Plan</th>
+                </tr>
+                
+                <tr>
+                    <td>
+                        B&aacute;sico
+                    </td>
+                    <td>
+                            1
+                    </td>
+                    <td>
+                        <span>RD$ 1,500 </span><a class="green-button" href="/usuario/comprar_plan/basico">Comprar</a>
+                        
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>
+                        Plus
+                    </td>
+                    <td>
+                            5
+                    </td>
+                    <td>
+                        <span>RD$ 6,000</span>  <a class="green-button" href="/usuario/comprar_plan/plus">Comprar</a>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>
+                        Agente
+                    </td>
+                    <td>
+                            10
+                    </td>
+                    <td>
+                        <span>RD$ 10,000</span> <a class="green-button" href="/usuario/comprar_plan/agente">Comprar</a>
+                        
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>
+                        Inmobiliaria
+                    </td>
+                    <td>
+                            25
+                    </td>
+                    <td>
+                       <span> RD$ 20,000 </span><a class="green-button" href="/usuario/comprar_plan/inmobiliaria">Comprar</a>                        
+                    </td>
+                </tr>
+                
+            </table>
+        
+        <p>Nota: Los precios mostrados en los planes incluyen el ITBIS.</p>
+        
+    </div>
+    
+</div>
