795b398384d7e9ac67361182ca0612458ee2a336
Ariel <ariel@ariel-desktop.(none)>
2011-11-08 18:25:52 -0400

    new banners

--- application/views/blocks/buy_blueprints_link.php
+++ application/views/blocks/buy_blueprints_link.php
@@ -1,6 +1,9 @@
 <a id="buy-blueprints-link">
-    OPORTUNIDAD <br/>
+    
+    <img alt="publicidad" src="/images/buttonAdvertising/2.jpg"/>
+    
+<!--    OPORTUNIDAD <br/>
     DE ADQUIRIR<br/>
     TU VIVIENDA<br/>
-    EN PLANOS<br/>
+    EN PLANOS<br/>-->
 </a>
