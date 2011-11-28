0179085bdeeb6739e2d9633bab9783861d79d75a
Ariel <ariel@ariel-desktop.(none)>
2011-11-28 16:51:44 -0400

    herramientas

---
+++ application/views/blocks/tools.php
@@ -0,0 +1,69 @@
+<div id="tools">
+    <h1>Conoce los beneficios de nuestras herramientas</h1>
+    <div id="tools-green-divisor"></div>
+    
+    <div id="buy-online-publications">
+        <div>
+            <h2 class="main-title">Compra publicaciones online</h2>
+            <p>Propiedom te ofrece múltiples plataformas para facilitarte el proceso de pago. Podrás comprar todas las publicaciones que quieras , cuando quieras y sin importar donde te encuentres ubicado. Nuestro portal está integrado con las plataformas de Paypal y Visanet, aceptando las tarjetas Visa y Mastercard.</p>
+        </div>
+        <img src="/images/tools/buy.png"/>
+    </div>
+    
+    <div id="realtime-publications">
+        <img src="/images/tools/publications.png" alt="publicaciones"/>
+        <div>
+            <h2 class="main-title">Publicaciones en tiempo real</h2>
+            <p>Desde el momento en que agregues una propiedad en tu cuenta,  tendrás la oportunidad de publicarla en tiempo real para que todos los usuarios puedan visitarla.</p>
+        </div>
+        
+    </div>
+    
+    <div id="panel">
+        
+        <div>
+            <h2 class="main-title">Panel administrativo</h2>
+            <p>Ya sea que tengas 1 o 25 propiedades, el panel administrativo te permite manejar tus propiedades con total libertad y mucho más.</p>
+            <ul>
+                <li>Crea, pulibca, modifica y elimina propiedades.</li>
+                <li>Compra publicaciones.</li>
+                <li>Resumen de las solicitudes hechas, propuestas recibidas y enviadas a terceros</li>
+                <li>Modifica tu informaci&oacute;n de contacto</li>
+                <li>Agrega otros agentes (S&oacute;lo para empresas)</li>
+            </ul>
+        </div>
+        <img src="/images/tools/panel.png" alt="solicitudes"/>
+    </div>
+    
+    
+    <div id="requests">
+        <img src="/images/tools/requests.png" alt="solicitudes"/>
+        <div>
+            <h2 class="main-title">Crea solicitudes y recibe propuestas.</h2>
+            <p>No importa si eres un visitante, agente o inmobiliaria, podrás utilizar una herramienta para realizar solicitudes de propiedades. Las solicitudes estarán disponibles para que cualquier usuario pueda enviarte propuestas potenciales.</p>
+        </div>
+        
+    </div>
+    
+    <div id="send-offers">
+                
+           
+        <div>
+            <h2 class="main-title">Env&iacute;a propuestas a terceros.</h2>
+            <p>Una vez que hayas comprado y publicado una propiedad, podrás responder a  solicitudes y enviar tu propiedad como potencial. Comunícate y dale seguimiento mediante mensajes que podrás enviar desde esta herramienta interactiva.</p>
+        </div>
+        <img src="/images/tools/sendOffers.png" alt="solicitudes"/>
+    </div>
+    
+    <div id="multimedia">
+        <img src="/images/tools/multimedia.png" alt="propiestas"/>
+        <div>
+            <h2 class="main-title">Recursos multimedia</h2>
+            <p>Para lograr una mejor apreciación de la propiedad, puedes agregar hasta 15 fotos por propiedad y un video. También, podrás ubicarla en la aplicación de google maps, para que los interesados tengan una referencia geográfica de la propiedad.</p>
+        </div>
+        
+    </div>
+    
+</div>
+    
+</div>
