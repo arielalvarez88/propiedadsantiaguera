868d6241a6d1e5f0f9841015d724c46f271d3bf7
xereu <xereu@xereu-laptop.(none)>
2011-11-28 15:25:39 -0400

    Agregado area de nosotros

---
+++ application/views/blocks/nosotros_view.php
@@ -0,0 +1,106 @@
+<div id="terminos-holder">
+<h2 class="main-title">Nosotros</h2>
+<p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma virtual creada con el propósito de conectar a la comunidad de agentes inmobiliarios con el mercado nacional, de una manera fácil, interactiva y colaborativa. 
+El equipo que conceptualizó la idea, quería facilitarles tanto a los agentes inmobiliarios como al público demandante de propiedades a nivel nacional, una herramienta que fuese:</p>
+<ul>
+<li>Completamente funcional, logrando maximizar el proceso de búsqueda y promoción de los inmubeles</li>
+<li>Totalmente instructiva, ya que el público necesita orientación informativa en el área inmobiliaria</li>
+<li>Creada para incrementar la efectividad de la comunicación directa entre los usuarios</li>
+<li>Visualmente atractiva e intuitiva, para presentar las propiedades con el mejor material disponible</li>
+</ul>
+<div class="form-divisor"></div>
+<h2 class="main-title">Equipo</h2>
+<p>Creemos que toda empresa debe manejarse por personas apasionadas por lo que hacen y comprometidas con entregar lo que saben hacer mejor en su campo de trabajo, a beneficio de todos los participantes involucrados con la empresa, especialmente a nuestros clientes.</p>
+
+<table id="tabla-equipo">
+    <tr>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el<br> mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el<br> mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el<br> mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+    </tr>
+        <tr>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+    </tr>
+        <tr>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+        <td>
+            <img src="/images/businessman.png" alt="business man"/>
+            <h4 class="main-title">Larry Cruz Minier</h4>
+            <h5 class="sub-title">Lic. en Publicidad</h5>
+            <p>Con su lanzamiento en el 2012, Propiedad Santiaguera es una plataforma 
+                virtual creada con el propósito de conectar a la comunidad de agentes 
+                inmobiliarios con el mercado nacional, de una manera fácil, interactiva 
+                y colaborativa.</p>
+        </td>
+    </tr>
+</table>
+
+
+</div>
