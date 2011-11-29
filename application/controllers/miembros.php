617d5aeb9d608cc7fc5a7535649be26e17f90118
Ariel <ariel@ariel-desktop.(none)>
2011-11-29 13:20:42 -0400

    user real info in miembros/ver/user_id, user real info in properties

--- application/controllers/miembros.php
+++ application/controllers/miembros.php
@@ -15,11 +15,26 @@ class Miembros extends CI_Controller
         $this->load->view('page',$data);
     }
     
-    public function ver($id)
+    public function ver($id=null)
     {
-        $data['topLeftSide'] = $this->load->view('blocks/user_info','',true);
+        
+        if(!$id || !is_numeric($id))
+            redirect ("/pagina_no_valida");
+        
+        $user_to_view = new User();
+        $user_to_view->where("id", $id)->get();
+        
+         if(!$user_to_view->id)
+            redirect ("/pagina_no_valida");
+        
+        
+         $user_info['user_to_view'] = $user_to_view;
+         
+        $data['topLeftSide'] = $this->load->view('blocks/user_info',$user_info,true);
         $data['topRightSide'] = $this->load->view('blocks/advertising','',true);
         $properties_pager_data['section'] = 'agents';
+        $properties_pager_data['properties'] = $user_to_view->property->where("display_property",1)->get()->all;
+        
         $data['bottomLeftSide'] = $this->load->view('blocks/properties_pager',$properties_pager_data,true);
         $this->load->view('page',$data);
     }
