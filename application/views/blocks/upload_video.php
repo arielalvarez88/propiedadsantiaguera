e0861f566ea0dd8fd47efd311efd1b55003711b2
Ariel <ariel@ariel-desktop.(none)>
2012-01-26 11:49:17 -0400

    oops

--- application/views/blocks/upload_video.php
+++ application/views/blocks/upload_video.php
@@ -6,7 +6,7 @@ $token = isset($token) ? $token : null;
 ?>
 
 
-<?php if ($post_url && $next_url && $tok): ?> 
+<?php if ($post_url && $next_url && $token): ?> 
     <form action="<?php echo $post_url; ?>?nexturl=<?php echo $next_url; ?>" method="post" enctype="multipart/form-data">
         <input name="file" type="file"/>
         <input name="token" type="hidden" value="<?php echo $token; ?>"/>
