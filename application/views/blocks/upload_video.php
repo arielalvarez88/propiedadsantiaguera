bfdbd70002f9dfc9c57dfe862f19a217fbe7921c
Ariel <ariel@ariel-desktop.(none)>
2012-01-26 11:31:39 -0400

    testing video upload

--- application/views/blocks/upload_video.php
+++ application/views/blocks/upload_video.php
@@ -1,5 +1,20 @@
-<form action="<?php echo $post_url;?>?nexturl=<?php echo $next_url;?>" method="post" enctype="multipart/form-data">
+<?php
+$video_url = isset($video_url) ? $video_url : null;
+$post_url = isset($post_url) ? $post_url : null;
+$next_url = isset($next_url) ? $next_url : null;
+$token = isset($token) ? $token : null;
+?>
+
+
+<?php if ($post_url && $next_url && $tok): ?> 
+    <form action="<?php echo $post_url; ?>?nexturl=<?php echo $next_url; ?>" method="post" enctype="multipart/form-data">
         <input name="file" type="file"/>
-        <input name="token" type="hidden" value="<?php echo $token;?>"/>
-       <input value="Upload Video File" type="submit" /> 
-</form>
+        <input name="token" type="hidden" value="<?php echo $token; ?>"/>
+        <input value="Upload Video File" type="submit" /> 
+    </form>
+<?php endif; ?>
+
+
+<?php if ($video_url): ?>
+    <iframe width="960" height="720" src="http://www.youtube.com/embed/7T-_Qz5qOhk" frameborder="0" allowfullscreen></iframe>
+<?php endif; ?>
