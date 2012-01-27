cb6d66fa3b0c667e04b5cb29fe3d79003a43ef0c
Ariel <ariel@ariel-desktop.(none)>
2012-01-27 18:26:53 -0400

    overlay_calculator

---
+++ application/views/blocks/overlay_property_calculator.php
@@ -0,0 +1,51 @@
+<div id="overlay-property-calculator-container" class="overlay-container">
+    
+<h1 class="overlay-header">Calculadora de cuotas para prestamo Hipotecario</h1>
+<div id="overlay-property-calculator" class="overlay-body">
+    
+    
+    <table>
+        <tr>
+            <th>
+                
+            </th>
+            <th>
+                Peso
+            </th>
+            
+            <th>
+                Dollar
+            </th>
+        </tr>
+        
+        <tr>
+            <td>
+                Monto a Financiar
+            </td>
+            <td>
+                <input id="overlay-property-calculator-dr" type="text" name="amont-dr"/>
+            </td>
+            
+            <td>
+                <input id="overlay-property-calculator-us" type="text" name="amont-us"/>
+            </td>
+        </tr>
+    </table>
+    
+    <div>
+        <span>Tasa de Interes</span> <input type="text" id="overlay-property-calculator-interest" name="interest"/>
+        <span>A&ntilde;os de financiamiento<br/> <span id="overlay-property-calculator-moths">12 meses</span> <input type="text" id="overlay-property-calculator-years" name="years"/>
+    </div>
+    
+    <p><a class="green-button" href="">Calcular</a></p>
+    <p>
+        <span id="overlay-property-calculator-montly-pay-title">Pago Mensual</span>
+        RD$<span id="overlay-property-calculator-rd-display"> </span>
+        US$<span id="overlay-property-calculator-us-display"> </span>
+    
+    </p>
+    
+    
+    
+</div>
+    </div>
--- application/views/blocks/property_calculator.php
+++ application/views/blocks/property_calculator.php
@@ -1,7 +1,7 @@
-<div id="property-calculator-container">
+<a class="no-decoration-anchor" href="/ajax/view_loader/blocks/overlay_property_calculator" id="property-calculator-container">
     <div id="property-calculator">
         <p>Calcula las cuotas mensuales</p>
         <span></span>
     </div>
     
-</div>
+</a>
