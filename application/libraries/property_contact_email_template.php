bd6fb288468b9b7ffbe9e200823d1dca2d72057c
Ariel <ariel@ariel-desktop.(none)>
2012-02-13 17:57:43 -0400

    logo miss

--- application/libraries/property_contact_email_template.php
+++ application/libraries/property_contact_email_template.php
@@ -42,7 +42,7 @@ class Property_contact_email_template  implements Iemail_template
         $html = <<<EOD
         <div style="background-color:#76bc3b; padding:40px; width:500px; height: 625px; text-align:center;">
             <div style="background-color: white; padding:30px; width:375px; height: 391px; position: relative; margin-right: auto; margin-left: auto;">
-                <img href="{$propiedom_logo}" alt="logo"/>
+                <img src="{$propiedom_logo}" alt="logo"/>
                 <div style="text-align:center;">
                     <h2 style="background-color:76bb3a;">Notificaci&oacute;n de Contacto</h2>
                     <p>La siguiente persona le ha enviadoun mensaje a trav&eacute;s de su propiedad <span style="font-weight:bold;">"{$this->property_name}" </span> No. de Referencia <span style="font-weight:bold;">#{$this->property_id}</span></p>
