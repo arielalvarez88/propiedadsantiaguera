f1b5ca5308f07af044d2cfb1c793d38951409f23
Ariel <ariel@ariel-desktop.(none)>
2012-02-15 18:39:09 -0400

    facebook share bad url

--- application/views/blocks/pdf_and_share.php
+++ application/views/blocks/pdf_and_share.php
@@ -14,7 +14,7 @@ $property = isset($property) ? $property : null
         <h2>Compartir </h2> 
         
         
-        <div class="fb-like" data-href="<?php echo base_url();?>ver/<?php echo $property->id;?>" data-send="true" data-layout="button_count" data-width="220" data-show-faces="true" data-action="recommend" data-font="arial"></div>                
+        <div class="fb-like" data-href="<?php echo base_url();?>/propiedades/ver/<?php echo $property->id;?>" data-send="true" data-layout="button_count" data-width="220" data-show-faces="true" data-action="recommend" data-font="arial"></div>                
         
 
         
