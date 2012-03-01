61d51809ea3e007e9aaa6eeae1aca223fa026cb5
Ariel <ariel@ariel-desktop.(none)>
2012-03-01 18:10:34 -0400

    adding Cuenta question to support section

---
+++ application/language/english/support_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$lang["support_account_question1"] = "¿Puedo cancelar mi cuenta en cualquier momento?";
+$lang["support_account_answer1"] = "Sí. Dentro del panel administrativo, en la sección de “cuenta” tendrá disponible una opción para cancelar su cuenta, en caso de que ya no tenga la necesidad de utilizar nuestros servicios.";
+
+$lang["support_account_question2"] = "¿Cómo puedo actualizar mi información de contacto?";
+$lang["support_account_answer2"] = 'Dirigiéndose al panel administrativo en la sección “Cuenta”, tendrá la facilidad de modificar o actualizar su información de contacto en cualquier momento.';
+
+
+$lang["support_account_question3"] = 'Soy un usuario registrado y no puedo accesar a mi cuenta.';
+$lang["support_account_answer3"] = 'En caso de que introduzca su e-mail y contraseña de manera correcta, y no pueda accesar a su cuenta, por favor comuníquese con nuestro equipo de soporte técnico “Contáctenos” para lograr solucionar su problema en la brevedad posible.';
+
+$lang["support_account_question4"] = 'Olvide mi contraseña y no puedo accesar a mi cuenta';
+$lang["support_account_answer4"] = 'No importa. Sólo seleccione la opción de “Olvidé mi contraseña” e introduzca su       e-mail nuevamente. Automáticamente le llegará una nueva contraseña para acceder a su cuenta.';
+
+
+?>
