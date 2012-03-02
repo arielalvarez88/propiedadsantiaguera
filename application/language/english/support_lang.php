fd452c9a49896013ae6b6d5db2ecf766679c273f
Ariel <ariel@ariel-desktop.(none)>
2012-03-02 10:17:19 -0400

    all support questions

--- application/language/english/support_lang.php
+++ application/language/english/support_lang.php
@@ -4,9 +4,25 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+$lang["support_faq_question1"] = "¿Cuáles métodos de pagos tienen disponibles?";
+$lang["support_faq_answer1"] = "Para lograr facilitarles el proceso, disponemos de varios métodos de pago; procesamos pagos en línea bajo las plataformas de PayPal y Visanet, aceptando las tarjetas Visa, Mastercard y American Express. También estamos disponibles para pagos presenciales dirigiéndose a nuestras oficinas.";
 
-$lang["support_account_question1"] = "¿Puedo cancelar mi cuenta en cualquier momento?";
-$lang["support_account_answer1"] = "Sí. Dentro del panel administrativo, en la sección de “cuenta” tendrá disponible una opción para cancelar su cuenta, en caso de que ya no tenga la necesidad de utilizar nuestros servicios.";
+$lang["support_faq_question2"] = "¿Cuándo comienzan a transcurrir los 30 días disponibles de la publicación?";
+$lang["support_faq_answer2"] = "Desde el momento en que usted compre una publicación y agregue la propiedad a su panel,  estará disponible para ser publicada y visitada por todos los usuarios que visiten el portal.  Cada propiedad tiene un contador regresivo el cual indicará la cantidad de días restantes a la publicación.";
+
+$lang["support_faq_question3"] = "¿Puedo cancelar mi cuenta en cualquier momento?";
+$lang["support_faq_answer3"] = "Sí. Dentro del panel administrativo, en la sección de “cuenta” tendrá disponible una opción para cancelar su cuenta, en caso de que ya no tenga la necesidad de utilizar nuestros servicios.";
+
+$lang["support_faq_question4"] = "Se me acabo el tiempo de la publicación, ¿Cómo hago para publicar mi propiedad otra vez?";
+$lang["support_faq_answer4"] = "Usted tiene la facilidad de activar la opción de  “auto-reactivación” la cual, automáticamente  transcurran los 30 días de su publicación, tomará un espacio de sus publicaciones restantes. En caso de no tener disponible publicaciones restantes,
+simplemente diríjase a la sección de cuentas de su panel y allí podrá comprar nuevamente la cantidad de publicaciones que desea.";
+
+$lang["support_faq_question5"] = "Tengo problemas subiendo las fotos";
+$lang["support_faq_answer5"] = "Debe verificar que las fotos cumplan con los formatos soportados. ( jpg, png o gif )  También debe confirmar que cada foto no exceda el tamaño de 4 MB. Recuerde que sólo tiene permitido subir 20 fotos por publicación.";
+
+
+$lang["support_account_question6"] = "Olvide mi contraseña y no puedo accesar a mi cuenta";
+$lang["support_account_answer6"] = "No importa. Sólo seleccione la opción de “Olvidé mi contraseña” e introduzca su       e-mail nuevamente. Automáticamente le llegará una nueva contraseña para acceder a su cuenta.";
 
 $lang["support_account_question2"] = "¿Cómo puedo actualizar mi información de contacto?";
 $lang["support_account_answer2"] = 'Dirigiéndose al panel administrativo en la sección “Cuenta”, tendrá la facilidad de modificar o actualizar su información de contacto en cualquier momento.';
@@ -17,6 +33,39 @@ $lang["support_account_answer3"] = 'En caso de que introduzca su e-mail y contra
 
 $lang["support_account_question4"] = 'Olvide mi contraseña y no puedo accesar a mi cuenta';
 $lang["support_account_answer4"] = 'No importa. Sólo seleccione la opción de “Olvidé mi contraseña” e introduzca su       e-mail nuevamente. Automáticamente le llegará una nueva contraseña para acceder a su cuenta.';
+
+
+$lang["support_payments_question1"] = "¿Cuáles métodos de pagos tienen disponibles?";
+$lang["support_payments_answer1"] = 'Para lograr facilitarles el proceso, disponemos de varios métodos de pago; procesamos pagos en línea bajo las plataformas de PayPal y Visanet, aceptando las tarjetas Visa, Mastercard y American Express. También estamos disponibles para pagos presenciales dirigiéndose a nuestras oficinas.';
+
+$lang["support_payments_question2"] = "¿Está el ITBIS incluido en el precio de los paquetes?";
+$lang["support_payments_answer2"] = 'Sí. Los precios detallados en sus respectivos paquetes incluyen el ITBIS.';
+
+$lang["support_payments_question3"] = "Compré un plan de publicaciones y no se me refleja en las publicaciones restantes.";
+$lang["support_payments_answer3"] = 'En caso de que no se actualicen sus publicaciones restantes, luego de comprar un paquete, por favor comuníquese con nuestro equipo de soporte técnico “Contáctenos” para lograr solucionar su problema en la brevedad posible.';
+
+$lang["support_listings_question1"] = "¿Cómo hago para publicar una propiedad?";
+$lang["support_listings_answer1"] = 'Sencillo, sólo debe comprar la cantidad de publicaciones que le gustaría publicar y se le habilitará un panel donde podrá agregar todas sus propiedades.
+Puede hacer la compra de las publicaciones en línea “Precios” o dirigiéndose a nuestra oficina.';
+
+$lang["support_listings_question2"] = "¿Cuándo comienzan a transcurrir los 30 días disponibles de la publicación?";
+$lang["support_listings_answer2"] = 'Desde el momento en que usted compre una publicación y agregue la propiedad a su panel,  estará disponible para ser publicada y visitada por todos los usuarios que visiten el portal.  Cada propiedad tiene un contador regresivo el cual indicará la cantidad de días restantes a la publicación.';
+
+$lang["support_listings_question2"] = "Se me acabo el tiempo de la publicación, ¿Cómo hago para publicar mi propiedad otra vez?";
+$lang["support_listings_answer2"] = 'Usted tiene la facilidad de activar la opción de  “auto-reactivación” la cual, automáticamente  transcurran los 30 días de su publicación, tomará un espacio de sus publicaciones restantes. En caso de no tener disponible publicaciones restantes,
+simplemente diríjase a la sección de cuentas de su panel y allí podrá comprar nuevamente la cantidad de publicaciones que desea.';
+
+$lang["support_listings_question3"] = "¿Puedo modificar o borrar una propiedad luego de estar publicada?";
+$lang["support_listings_answer3"] = 'Una vez la propiedad este publicada, tendrá la opción de poder editar las características, fotos y video. De la misma forma, también podrá eliminar la publicación. Al momento de crear una propiedad,  éstas se irán acumulando en la sección de “Propiedades Creadas”, donde el usuario podrá editar esas propiedades y elegir aquellas que desea publicar. ';
+
+$lang["support_listings_question4"] = "Tengo problemas subiendo las fotos";
+$lang["support_listings_answer4"] = 'Debe verificar que las fotos cumplan con los formatos soportados. ( jpg, png o gif )  También debe confirmar que cada foto no exceda el tamaño de 4 MB. Recuerde que sólo tiene permitido subir 20 fotos por publicación.';
+
+$lang["support_listings_question4"] = "Tengo problemas subiendo el video";
+$lang["support_listings_answer4"] = 'Debe verificar que las fotos cumplan con los formatos soportados. ( jpg, png o gif )  También debe confirmar que el video no exceda el tamaño de 4 MB. Recuerde que sólo tiene permitido subir 20 fotos por publicación.';
+
+$lang["support_listings_question5"] = "Ya vendí o alquilé mi propiedad, ¿cómo la elimino de Propiedom?";
+$lang["support_listings_answer5"] = 'Fácil, diríjase a sección de Propiedades en su panel, vaya a “creadas” y simplemente elimine la propiedad que usted quiera.';
 
 
 ?>
