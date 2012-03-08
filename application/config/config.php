29cdd41baa2b299873f7dc9948262b12047e9896
Jose Infante <jinfante@crusaders.dreamhost.com>
2012-03-08 15:30:58 -0800

    i dont know

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://local.propiedadsantiaguera.com';
+$config['base_url']	= 'http://www.propiedom.com';
 
 /*
 |--------------------------------------------------------------------------
--- application/libraries/Environment_vars.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Environment_vars {    
-    public static $google_api = array("developer_id" => "AI39si58bPU3fVlTrF8i4e$");
-    public static $youtube= array("username" => "arielalvarez88@gmail.com");
-
-    public static $environment = "production";
-}    
